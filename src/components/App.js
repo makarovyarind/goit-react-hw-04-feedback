c9185d68d99b6c98f1692f3b598fa76100165425
makarovyarind <1makarov.yaroslav@gmail.com>
2023-06-07 23:05:51 +0300

    1

--- src/components/App.js
+++ src/components/App.js
@@ -12,7 +12,7 @@ export function App() {
   const onLeaveFeedback = option => {
     setstatus(prevStatus => ({
       ...prevStatus,
-      [option]: prevStatus[option] + 1,
+      [option]: prevStatus[option] + 1, 
     }));
   };
 
